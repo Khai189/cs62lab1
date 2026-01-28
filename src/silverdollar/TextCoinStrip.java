393d244980dc50e75836abfaa9922e069ad5b251
Khai189 <khmr2024@mymail.pomona.edu>
2026-01-28 15:31:39 -0800

    Style touches!

--- src/silverdollar/TextCoinStrip.java
+++ src/silverdollar/TextCoinStrip.java
@@ -78,9 +78,11 @@ public class TextCoinStrip {
      * @return true if the move is legal
      */
     public boolean isLegalMove(int start, int distance) {
+        // first we check some conditions; is the starting move actually a puck, is the distance valid, is it possible to move there?
         if(start >= theStrip.size() || start < 0 || start - distance < 0 || distance == 0 || !theStrip.get(start)){
             return false;
         }
+        // we check to see if there are any pucks in our way we need to skip over
         for(int i = 1; i<=distance;i++){
             if(theStrip.get(start-i)){
                 return false;
@@ -96,9 +98,13 @@ public class TextCoinStrip {
      * @param distance how far the coin is to move pre-condition: the move must be a legal one
      */
     public void makeMove(int start, int distance) {
+        // we remove the original element
         theStrip.remove(start);
+        // we then replace it with something empty
         theStrip.add(start, false);
+        // we remove the new element
         theStrip.remove(start-distance);
+        // we then replace it with a new puck
         theStrip.add(start-distance, true);
     }
 
@@ -108,13 +114,16 @@ public class TextCoinStrip {
      * @return true if there are no more moves
      */
     public boolean gameIsOver() {
+        // first, we set a boolean in place to check if all falses are on the right
         boolean rightHandSide = false;
+        // we check the Strip to see if there's a true
         for(int i = theStrip.size() - 1; i>=0;i--){
             if(rightHandSide){
                 if(!theStrip.get(i)){
                     return false;
                 }
             }
+            // if we get a puck, we start asking if only pucks are after it. If there's a false after a puck, clearly all pucks aren't on the left yet
             if(theStrip.get(i)){
                 rightHandSide = true;
             }
